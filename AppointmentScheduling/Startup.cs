387a9c0ef3fd3ac17e79c142316ac41b844481b4
Paras Rawat <parasrawat67@gmail.com>
2022-01-15 19:33:14 +0530

    Configuring Dependency Service for Application User

--- AppointmentScheduling/Startup.cs
+++ AppointmentScheduling/Startup.cs
@@ -30,7 +30,7 @@ namespace AppointmentScheduling
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddControllersWithViews();
-            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
