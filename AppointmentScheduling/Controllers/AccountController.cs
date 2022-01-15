735d0ec1c8f010f6f7e918c19ded2acdc64c0f18
Paras Rawat <parasrawat67@gmail.com>
2022-01-15 19:45:45 +0530

    Register Post Action Method

--- AppointmentScheduling/Controllers/AccountController.cs
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using AppointmentScheduling.Models;
+using AppointmentScheduling.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,10 +12,17 @@ namespace AppointmentScheduling.Controllers
     public class AccountController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _UserManger;
+        private readonly SignInManager<ApplicationUser> _SignInManager;
+        private readonly RoleManager<IdentityRole> _RoleManager;
 
-        public AccountController(ApplicationDbContext db)
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> UManger, 
+            SignInManager<ApplicationUser> SManager, RoleManager<IdentityRole> RManager)
         {
             _db = db;
+            _UserManger = UManger;
+            _SignInManager = SManager;
+            _RoleManager = RManager;
         }
         public IActionResult Index()
         {
@@ -26,6 +35,27 @@ namespace AppointmentScheduling.Controllers
         }
         public IActionResult Register()
         {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName=model.Email,
+                    Email=model.Email,
+                    Name=model.Name
+                };
+                var result = await _UserManger.CreateAsync(user);
+                if(result.Succeeded)
+                {
+                    await _SignInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
             return View();
         }
     }
