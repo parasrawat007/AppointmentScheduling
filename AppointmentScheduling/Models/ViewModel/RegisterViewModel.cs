2ca7e7e7c70ce1d46f655cf202ddeaae878c4aa6
Paras Rawat <parasrawat67@gmail.com>
2022-01-15 12:33:10 +0530

    Adding Register View Model

---
+++ AppointmentScheduling/Models/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Models.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+       
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Required]
+        [StringLength(100,ErrorMessage ="The {0} must be at least {2} characters long.",MinimumLength =6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Password and Confirm Password do not match.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
