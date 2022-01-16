e7675af5ea857d7513c9704c6ce9a9cece38f3fc
Paras Rawat <parasrawat67@gmail.com>
2022-01-16 20:02:35 +0530

    Adding and using Full Calendar

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,20 @@
-﻿
+﻿$(document).ready(function () {
+    InitializeCalendar();
+});
+function InitializeCalendar(){
+    try {
+        $("#calendar").fullCalendar({
+            timezone: false,
+            header: {
+                left: "prev,next,today",
+                center: "title",
+                right:"month,agendaWeek,agendaDay"
+            },
+            selectTable: true,
+            editable: false             
+        });
+    }
+    catch (e) {
+        alert(e);
+    }
+}
