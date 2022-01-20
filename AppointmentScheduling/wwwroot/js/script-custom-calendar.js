330f202c0c5412616ca18cc9607e18b90acdd788
Paras Rawat <parasrawat67@gmail.com>
2022-01-20 21:50:39 +0530

    Updating Full Calender

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -3,19 +3,37 @@
 });
 function InitializeCalendar(){
     try {
-        $("#calendar").fullCalendar({
-            timezone: false,
-            header: {
-                left: "prev,next,today",
-                center: "title",
-                right:"month,agendaWeek,agendaDay"
-            },
-            selectable: true,
-            editable: false,
-            select: function (event) {
-                onShowModal(event, null);
-            }
-        });
+        
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: "prev,next,today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay"
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+            calendar.render();
+      
+
+        //$("#calendar").fullCalendar({
+        //    timezone: false,
+        //    header: {
+        //        left: "prev,next,today",
+        //        center: "title",
+        //        right:"month,agendaWeek,agendaDay"
+        //    },
+        //    selectable: true,
+        //    editable: false,
+        //    select: function (event) {
+        //        onShowModal(event, null);
+        //    }
+        //});
     }
     catch (e) {
         alert(e);
