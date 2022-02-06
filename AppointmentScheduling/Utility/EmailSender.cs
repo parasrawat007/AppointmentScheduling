8e968a57c904ed28fe4907518ab865a509cb184c
Paras Rawat <parasrawat67@gmail.com>
2022-02-06 22:24:24 +0530

    Installing Microsoft.AspNetCore.Identity.UI And Mail Jet and Intialising Email Sender

---
+++ AppointmentScheduling/Utility/EmailSender.cs
@@ -0,0 +1,39 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("e73fb71f954b2963ed687c1b444057c7","f8d2cb375114642c70334971d28413b1");
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+               .Property(Send.Messages, new JArray {
+                new JObject {
+                 {"From", new JObject {
+                  {"Email", "parasrawat67@gmail.com"},
+                  {"Name", "Appointment Scheduler"}
+                  }},
+                 {"To", new JArray {
+                  new JObject {
+                   {"Email",email},
+                   }
+                  }},
+                 {"Subject",subject},               
+                 {"HTMLPart", htmlMessage}
+                 }
+                   });
+            MailjetResponse response = await client.PostAsync(request);
+        }
+    }
+}
